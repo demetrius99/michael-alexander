0d2a24252cc32619e8e2aff96b002db778c3dd5e
Demetrius Dasilva <34225685+demetrius99@users.noreply.github.com>
2019-02-22 01:42:19 -0600

    Delete elizondo.js

--- elizondo.js
+++
@@ -1,5 +0,0 @@
-<!-- Calendly badge widget begin -->
-<link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
-<script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript"></script>
-<script type="text/javascript">Calendly.initBadgeWidget({url: 'https://calendly.com/demetrius_dasilva', text: 'Schedule time with Demetrius', color: '#00a2ff', branding: false});</script>
-<!-- Calendly badge widget end -->		
